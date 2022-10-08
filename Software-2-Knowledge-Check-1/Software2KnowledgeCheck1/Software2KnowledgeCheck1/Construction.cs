2783959284315804d6ef933168bb1fdcc5467a61
Liam Powers <33320954+PowersLiam@users.noreply.github.com>
2022-10-07 22:31:20 -0400

    Add files via upload

---
+++ Software-2-Knowledge-Check-1/Software2KnowledgeCheck1/Software2KnowledgeCheck1/Construction.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software2KnowledgeCheck1
+{
+    internal class Construction
+    {
+        public void CreateSpecific<T>(T specific, City city) where T : Building
+        {
+            // Get materials
+            var materialRepo = new MaterialsRepo();
+            var materialsNeeded = materialRepo.GetMaterials();
+
+            var permitRepo = new ZoningAndPermitRepo();
+            var buildingWasMade = false;
+
+            if(specific is Apartment) { buildingWasMade = ConstructBuilding<Apartment>(materialsNeeded, permitRepo.GetPermit(), permitRepo.ZoningApproves()); }
+            else if (specific is HighRise) { buildingWasMade = ConstructBuilding<HighRise>(materialsNeeded, permitRepo.GetPermit(), permitRepo.ZoningApproves()); }
+            else { buildingWasMade = false; }
+
+            if (buildingWasMade)
+            {
+                city.Buildings.Add(specific);
+            }
+        }
+
+        public bool ConstructBuilding<T>(List<string> materials, bool permit, bool zoning) where T : Building
+        {
+            if (permit && zoning)
+            {
+                foreach (var material in materials)
+                {
+                    if (material == "concrete")
+                    {
+                        // start laying foundation
+                    }
+                    else if (material == "Steel")
+                    {
+                        // Start building structure
+                    }
+                    // etc etc...
+
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
